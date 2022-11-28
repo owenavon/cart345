3e593b6de63df0bd9f313f0d9c873cab29361e9a
owenavon <89999663+owenavon@users.noreply.github.com>
2022-11-28 12:18:33 -0500

    Used JQuery .scroll
    Text changes upon user scroll, dialog box is set to proper dimensions, and initial structure of setInterval is written for several dialog boxes to "pop up"

--- creative/js/script.js
+++ creative/js/script.js
@@ -2,16 +2,28 @@ $(document).ready(function() {
 
     $("#dialog").dialog({
         autoOpen: false,
-        
+        modal: true,
+        width: 600,
+        height: 515
     }).parent().resizable({
         containment: "body",
         disabled: true
     }).draggable({
         containment: "body",
         disabled: true
-
     });
-    
+
+
+    $("#dialog-test").dialog({
+        autoOpen: false,
+    }).parent().resizable({
+        containment: "body",
+        disabled: true
+    }).draggable({
+        containment: "body",
+        disabled: false
+    });
+
 
     $(document).keypress(function(event) {
         let keycode = (event.keyCode ? event.keyCode : event.which);
@@ -24,16 +36,41 @@ $(document).ready(function() {
           $("#dialog").dialog("open");
           $("#content-area").hide();
 
+            $("#dialog-content").scroll(function () {
+                if ($(this).scrollTop() > 25) {
+                    // New Words
+                    $("#changeOne").text("today")
+                    $("#changeTwo").text("hi")
+                    $("#changeThree").text("secret")
+                } 
+                else {
+                    // Original Words
+                    $("#changeOne").text("ipsum")
+                    $("#changeTwo").text("sit")
+                    $("#changeThree").text("leo")
+                }
+            });
         }
     });
 
+
     $( "#accept" ).click(function() {
         $("#dialog").remove();
+
+        let i = 0;
+        // This block will be executed 100 times.
+        setInterval(function() {
+            if (i == 5) {
+                clearInterval(this);
+            } 
+            else {
+                console.log('Currently at ' + (i++));
+                 $("#dialog-test").dialog("open");
+            } 
+        }, 1000);         
     });
 
-
-
-
     
+
 
 });
